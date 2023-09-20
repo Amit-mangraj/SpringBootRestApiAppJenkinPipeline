850fa83e958c7d4b450cd68d6632a0ecf76bc28a
amit-mangaraj <Amitkumar.mangaraj@ltts.com>
2023-09-21 02:24:18 +0530

    first commit

---
+++ src/test/java/com/springboot/restApi/RestApidemo/RestApidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restApi.RestApidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
